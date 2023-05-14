367df94fc5c403fd02ce20b49937d32c93d35023
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-05-14 14:22:12 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,10 +1,3 @@
-variable "aws_access_key" {
-default = "AKIATG57XZYFCICUWHG7"
-}
-variable "aws_secret_key" {
-default = "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"
-}
-variable "region" {
-        default = "us-west-1"
-}
-
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
